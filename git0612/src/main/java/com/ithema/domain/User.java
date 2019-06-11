34dc4b50b3e6d1aea16ce68bf0c84877a456a049
baijun <baijun@itcast.cn>
2019-06-12 01:08:17 +0800

    second

--- git0612/src/main/java/com/ithema/domain/User.java
+++ git0612/src/main/java/com/ithema/domain/User.java
@@ -3,4 +3,12 @@ package com.ithema.domain;
 public class User {
     private String name;
     private String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
